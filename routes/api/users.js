2ea359b7f163e71c7fca53449848b223bfdb5ef0
Frank Mendez <frankmendezwebdev@gmail.com>
2020-02-20 11:48:26 +0800

    User registration with JWT

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const User = require('../../models/User');
 
@@ -51,11 +53,22 @@ router.post(
 
       user.password = await bcrypt.hash(password, salt);
 
-      await user.save();
+      //Return token
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
 
-      //Return jsonwebtoken
-
-      res.send('User registered');
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.log(err.message);
       res.status(500).send('Server Error');
