54f12610fb973b85cf914b8d15220a7ef4c41890
Frank Mendez <frankmendezwebdev@gmail.com>
2020-02-21 20:57:45 +0800

    Get Profile by User ID

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -125,4 +125,25 @@ router.get('/', async (req, res) => {
   }
 });
 
+// @route GET api/profile/user/:user_id
+// @desc Get Profile by User ID
+// @access Public
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate('user', ['name', 'date']);
+
+    if (!profile) return res.status(400).json({ msg: 'Profile not found' });
+
+    return res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
