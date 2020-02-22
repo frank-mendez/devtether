d27effb7e8b3d496a66f1c06f0d5f1ed1fedc6d6
Frank Mendez <frankmendezwebdev@gmail.com>
2020-02-22 23:05:32 +0800

    Add and Update Post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,87 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
 
-// @route GET api/posts
-// @desc Test Auth Route
-// @access Public
-router.get('/', (req, res) => res.send('Post Route'));
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route  POST api/posts
+// @desc   Create a post
+// @access Private
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('text', 'Text is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      //Get the logged in user through user id
+      const user = await User.findById(req.user.id).select('-password');
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      });
+
+      const post = await newPost.save();
+
+      //Return the new created post
+      return res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+// @route  PUT api/posts/:post_id
+// @desc   Update a post
+// @access Private
+router.put(
+  '/:post_id',
+  [
+    auth,
+    auth,
+    [
+      check('text', 'Text is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      let post = await Post.findOneAndUpdate(
+        { _id: req.params.post_id },
+        { text: req.body.text, edited: true }
+      );
+
+      if (!post) return res.status(400).json({ msg: 'Post not found' });
+
+      return res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
