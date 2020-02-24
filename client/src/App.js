e63d13dd1a3652c15649590e4a59d643cb7e937e
Frank Mendez <frankmendezwebdev@gmail.com>
2020-02-24 18:24:26 +0800

    Added SB Admin Bootstrap Theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Sidebar from './components/layouts/Sidebar';
+import Navbar from './components/layouts/Navbar';
+import Main from './components/layouts/Main';
 import './App.css';
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>Hello</h1>
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <Fragment>
+      <div id='wrapper'>
+        <Sidebar />
+        <div id='content-wrapper' className='d-flex flex-column'>
+          <Navbar />
+          <Main />
+        </div>
+      </div>
+    </Fragment>
+  </Router>
+);
 
 export default App;
