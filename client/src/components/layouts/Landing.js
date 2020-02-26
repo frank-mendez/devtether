9ca3b59089060be8ef56b8d3c10960de16e7bce4
Frank Mendez <frankmendezwebdev@gmail.com>
2020-02-27 00:26:20 +0800

    Logout & Navbar Links

--- client/src/components/layouts/Landing.js
+++ client/src/components/layouts/Landing.js
@@ -29,14 +29,14 @@ const Landing = () => {
                 </nav>
                 <div className='row justify-content-md-center landing-header p-50'>
                     <div className='col-md-8 text-center'>
-                        <img className='img-fluid' src='https://res.cloudinary.com/https-frank-mendez-github-io/image/upload/v1582724282/devtether/img/undraw_welcome_cats_thqn.svg' />
+                        <img alt='svg graphics'className='img-fluid' src='https://res.cloudinary.com/https-frank-mendez-github-io/image/upload/v1582724282/devtether/img/undraw_welcome_cats_thqn.svg' />
                         <h1>Welcome to DevTether</h1>
                         <h6>Create custom post, measure social engagement, monitor and connect to other developers, publish collaborations, and attend live events</h6>
                     </div>
                 </div>
                 <div className='row p-50'>
                     <div className='col-xs-12 col-lg-6'>
-                    <img className='img-fluid' src='https://res.cloudinary.com/https-frank-mendez-github-io/image/upload/v1582724365/devtether/img/undraw_public_discussion_btnw.svg' />
+                    <img alt='svg graphics' className='img-fluid' src='https://res.cloudinary.com/https-frank-mendez-github-io/image/upload/v1582724365/devtether/img/undraw_public_discussion_btnw.svg' />
                     </div>
                     <div className='col-xs-12 col-lg-6'>
                         <h2 className='mt-50'>Create custom post</h2>
@@ -49,22 +49,22 @@ const Landing = () => {
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     </div>
                     <div className='col-xs-12 col-lg-6'>
-                    <img className='img-fluid' src='https://res.cloudinary.com/https-frank-mendez-github-io/image/upload/v1582724449/devtether/img/undraw_lost_online_wqob.svg' />
+                    <img alt='svg graphics' className='img-fluid' src='https://res.cloudinary.com/https-frank-mendez-github-io/image/upload/v1582724449/devtether/img/undraw_lost_online_wqob.svg' />
                     </div>
                 </div>
                 <div className='row p-50 section-3'>
                     <div className='col-xs-12 col-lg-4'>
-                    <img className='img-fluid' src='https://res.cloudinary.com/https-frank-mendez-github-io/image/upload/v1582725738/devtether/img/undraw_meet_the_team_e5b7.svg' />
+                    <img alt='svg graphics' className='img-fluid' src='https://res.cloudinary.com/https-frank-mendez-github-io/image/upload/v1582725738/devtether/img/undraw_meet_the_team_e5b7.svg' />
                     <h4 className='mt-50'>Monitor and connect to other developers</h4>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     </div>
                     <div className='col-xs-12 col-lg-4'>
                     <h4 className='mt-50'>Publish collaborations</h4>
                     <p>Lorem ipsum dolor sit amet, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    <img className='img-fluid' src='https://res.cloudinary.com/https-frank-mendez-github-io/image/upload/v1582725846/devtether/img/undraw_shared_workspace_hwky.svg' />
+                    <img alt='svg graphics' className='img-fluid' src='https://res.cloudinary.com/https-frank-mendez-github-io/image/upload/v1582725846/devtether/img/undraw_shared_workspace_hwky.svg' />
                     </div>
                     <div className='col-xs-12 col-lg-4'>
-                    <img className='img-fluid' src='https://res.cloudinary.com/https-frank-mendez-github-io/image/upload/v1582725818/devtether/img/undraw_remotely_2j6y.svg' />
+                    <img alt='svg graphics' className='img-fluid' src='https://res.cloudinary.com/https-frank-mendez-github-io/image/upload/v1582725818/devtether/img/undraw_remotely_2j6y.svg' />
                     <h4 className='mt-50'>Attend live events</h4>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,  in culpa qui officia deserunt mollit anim id est laborum.</p>
                     </div>
