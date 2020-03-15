4a4c0aa249597b7e0fe62d70e7753243a808a6cc
Frank Mendez <frankmendezwebdev@gmail.com>
2020-03-15 19:32:27 +0800

    feat(elastic): Setup Elastic UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import '@elastic/eui/dist/eui_theme_light.css';
 import './App.css';
+import Login from './components/pages/Login';
+import Register from './components/pages/Login';
+import Dashboard from './components/pages/Dashboard';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Router>
+        <Switch>
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/' component={Dashboard} />
+        </Switch>
+      </Router>
+    </Fragment>
   );
 }
 
