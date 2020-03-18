f949dec0866070f8cc573b0ddbe1ef6a0cd3b79b
Frank Mendez <frankmendezwebdev@gmail.com>
2020-03-18 22:01:09 +0800

    feat(login): Added Login UI

--- client/src/components/pages/auth/Login.js
+++ client/src/components/pages/auth/Login.js
@@ -1,7 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  EuiPage,
+  EuiPageBody,
+  EuiPageContent,
+  EuiPageContentBody,
+  EuiPageContentHeader,
+  EuiPageContentHeaderSection,
+  EuiTitle,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiFieldText,
+  EuiFieldPassword,
+  EuiCallOut,
+  EuiButton
+} from '@elastic/eui';
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onChangeEmail = e => {
+    setEmail(e.target.value);
+  };
+
+  const onChangePassword = e => {
+    setPassword(e.target.value);
+  };
+
+  return (
+    <EuiPage style={{ height: '700px' }}>
+      <EuiPageBody>
+        <EuiPageContent
+          style={{ width: '400px', textAlign: 'center' }}
+          verticalPosition='center'
+          horizontalPosition='center'
+        >
+          <EuiPageContentHeader
+            style={{ display: 'block', textAlign: 'center' }}
+          >
+            <EuiPageContentHeaderSection>
+              <EuiTitle>
+                <h2>Welcome to DevTether</h2>
+              </EuiTitle>
+            </EuiPageContentHeaderSection>
+          </EuiPageContentHeader>
+          <EuiPageContentBody>
+            <EuiFlexGroup>
+              <EuiFlexItem>
+                <EuiCallOut
+                  title='Invalid Credentials'
+                  color='danger'
+                  iconType='alert'
+                ></EuiCallOut>
+              </EuiFlexItem>
+            </EuiFlexGroup>
+            <EuiFlexGroup>
+              <EuiFlexItem>
+                <EuiFieldText
+                  icon='user'
+                  value={email}
+                  onChange={e => onChangeEmail(e)}
+                  placeholder='Email'
+                />
+              </EuiFlexItem>
+            </EuiFlexGroup>
+            <EuiFlexGroup>
+              <EuiFlexItem>
+                <EuiFieldPassword
+                  placeholder='Password'
+                  value={password}
+                  onChange={e => onChangePassword(e)}
+                />
+              </EuiFlexItem>
+            </EuiFlexGroup>
+            <EuiFlexGroup>
+              <EuiFlexItem>
+                <EuiButton fill isLoading={false}>
+                  Login
+                </EuiButton>
+              </EuiFlexItem>
+            </EuiFlexGroup>
+          </EuiPageContentBody>
+        </EuiPageContent>
+      </EuiPageBody>
+    </EuiPage>
+  );
 };
 
 export default Login;
